a90b440f3e1cb69a13f1776cd47dbec0e5936709
acxel.aplicano <acxel.aplicano@unah.hn>
2024-07-28 15:03:35 -0600

    incluir jetstream

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('livewire.calculadora');
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    
 });
