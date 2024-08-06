9df9fc35b97244961389bb9a20b1cb28dfc7f0a5
vanessa <vanessa.lainez@unah.hn>
2024-08-06 16:30:12 -0600

    cambios de acxel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio - BooleanCalc</title>
+    <title>Inicio - Calculadora Booleana</title>
     <!-- Flowbite CSS CDN -->
     <link href="https://unpkg.com/flowbite@latest/dist/flowbite.min.css" rel="stylesheet">
     <!-- Tailwind CSS CDN -->
