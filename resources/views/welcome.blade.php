b2464674dcf7e7c8109b3f104b142773d2cce5bb
acxel.aplicano <acxel.aplicano@unah.hn>
2024-08-12 10:28:21 -0600

    Arreglos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,19 +32,19 @@
 
 <body>
     <!-- Barra de menú -->
-    <nav class="bg-blue-500 text-white p-4 fixed top-0 w-full z-10">
+    <nav class="bg-gray-500 text-white p-4 fixed top-0 w-full z-10">
 
         <div>
-            <a href="/" class="text-xl letra font-bold">Calculadora Binaria</a>
-            <a href="/dashboard" class="px-4 letra py-2 hover:bg-blue-700 rounded">Operaciones</a>
-            <a href="/conversiones" class="px-4 letra py-2 hover:bg-blue-700 rounded">Conversiones</a>
+            <a href="/" class="text-xl letra font-bold">BinaryOps</a>
+            <a href="/dashboard" class="px-4 letra py-2 hover:bg-gray-700 rounded">Operaciones</a>
+            <a href="/conversiones" class="px-4 letra py-2 hover:bg-gray-700 rounded">Conversiones</a>
         </div>
     </nav>
 
     <!-- Contenido principal -->
     <div class="pt-16">
         <div class="flex flex-col items-center justify-center min-h-screen">
-            <h1 class="text-4xl font-bold letra mb-6">¡Bienvenido a la Calculadora Binaria!</h1>
+            <h1 class="text-4xl font-bold letra mb-6">¡Bienvenido a BinaryOps!</h1>
             <p class="text-lg letra mb-8">Seleccione una opción para comenzar:</p>
             <div class="flex space-x-4">
             <button><a href="/dashboard"
